bff8ef12a4e8929fbc63a00cbe092ef3a748eeed
Evgenii Prikazchikov <prikazchikov.ea@edu.sbpstu.ru>
2021-10-07 21:29:25 +0300

    Three tasks solved

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -90,7 +90,7 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(x2 - x1) + sqr(y2 - y1))
 
 /**
  * Простая (2 балла)
@@ -98,7 +98,11 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int  {
+    val x1 = number / 100
+    val x2 = x1 % 10
+    return x2
+}
 
 /**
  * Простая (2 балла)
