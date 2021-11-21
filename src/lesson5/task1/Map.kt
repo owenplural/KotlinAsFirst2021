e12a2d2fd156d46cce0b05b60c78439f0cb8dd4d
Evgenii Prikazchikov <prikazchikov.ea@edu.sbpstu.ru>
2021-11-22 00:20:52 +0300

    second task solved

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -1,4 +1,4 @@
-@file:Suppress("UNUSED_PARAMETER", "ConvertCallChainIntoSequence")
+ @file:Suppress("UNUSED_PARAMETER", "ConvertCallChainIntoSequence")
 
 package lesson5.task1
 
@@ -108,8 +108,13 @@ fun buildGrades(grades: Map<String, Int>): Map<Int, List<String>> = TODO()
  *   containsIn(mapOf("a" to "z"), mapOf("a" to "z", "b" to "sweet")) -> true
  *   containsIn(mapOf("a" to "z"), mapOf("a" to "zee", "b" to "sweet")) -> false
  */
-fun containsIn(a: Map<String, String>, b: Map<String, String>): Boolean = TODO()
-
+fun containsIn(a: Map<String, String>, b: Map<String, String>): Boolean {
+    for ((key) in a) {
+        if (a[key] == b[key])
+            return true
+    }
+    return false
+}
 /**
  * Простая (2 балла)
  *
