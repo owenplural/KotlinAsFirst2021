8c7174ef2419f85cd98d423f2f05a16b7a15420b
Evgenii Prikazchikov <hellohome0@gmail.com>
2022-02-01 13:02:41 +0300

    deleted one task

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -74,15 +74,7 @@ fun main() {
  * Обратите внимание: некорректная с точки зрения календаря дата (например, 30.02.2009) считается неверными
  * входными данными.
  */
-fun dateStrToDigit(str: String): Int {
-    val parts = str.split(":")
-    var result = 0
-    for (part in parts) {
-        val number = part.toInt()
-        result = result * 60 + number
-    }
-    return result
-}
+fun dateStrToDigit(str: String): Int = TODO()
 
 
 /**
